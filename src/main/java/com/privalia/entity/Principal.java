6d9a015aa66e169f9c6bd1d7c3cfbcc8ca43048d
albert.ferriz <albert.ferriz@privalia.com>
2017-10-10 18:43:21 +0200

    uncommited change

--- src/main/java/com/privalia/entity/Principal.java
+++ src/main/java/com/privalia/entity/Principal.java
@@ -1,13 +1,14 @@
 package com.privalia.entity;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		new ClassPathXmlApplicationContext("spring-config.xml");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
 		
 		context.scan("com.privalia.presentation.annotations2");
 		context.refresh();
